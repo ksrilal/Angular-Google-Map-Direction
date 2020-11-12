e7bfa43b88668a5f026458566c6883d90ff95a0d
ksrilal <ksrilalranasinghe95@gmail.com>
2020-11-12 13:43:39 +0530

    ksrilal_20201112

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,8 @@ export class AppComponent {
   origin: any;
   destination: any;
   minDestinationIndex: any;
-  duration: any;
+  duration: number = 0;
+  durationText: any;
   tempLocationList = [];
   public waypoints = [];
   markers = [];
@@ -40,33 +41,17 @@ export class AppComponent {
 
   dbLocationsList = [    
     // { lat: 6.9044, lng: 79.8540 }, //bambalapitiya
-    {lat: 6.897558259656697, lng: 79.86007655452623}, //thunmulla
-    //{ lat: 6.9117, lng: 79.8646 }, //Cinnamon Gardens
+    // {lat: 6.897558259656697, lng: 79.86007655452623}, //thunmulla
+    // { lat: 6.9117, lng: 79.8646 }, //Cinnamon Gardens
     { lat: 6.8976, lng: 79.8815 }, //narahenpita
-    //{lat: 6.881978742791475, lng: 79.85887818038464}, //scienter
-    { lat: 6.8741, lng: 79.8605 }, //Wellawatte 
-    //{ lat: 6.9094, lng: 79.8943 }, //Rajagiriya
+    // {lat: 6.881978742791475, lng: 79.85887818038464}, //scienter
+    // { lat: 6.8741, lng: 79.8605 }, //Wellawatte 
+    { lat: 6.9094, lng: 79.8943 }, //Rajagiriya
   ]; 
 
 
   ngOnInit() {
-    this.getDirection();
-
-    var service = new google.maps.DirectionsService();
-    service.route({
-      origin: { lat: 6.908716852475053, lng: 79.87734690661742},
-      destination: this.tempLocationList[this.tempLocationList.length-1],
-      travelMode: google.maps.TravelMode.DRIVING ,
-      avoidHighways: true,
-      avoidTolls: true,
-    }, (data, status) => {
-      if (status === 'OK') {
-        this.duration = data.routes[0].legs[0].duration.text;
-      } else {
-         console.log("your error")
-      }
-    });
-    
+    this.getDirection();  
   }
 
   getDirection() {
@@ -76,33 +61,51 @@ export class AppComponent {
   }
    
   calculateDistance() {
+    var service = new google.maps.DirectionsService();
     this.tempLocationList.push({ lat: 6.908716852475053, lng: 79.87734690661742}); //borella (origin)
     const distanceList: number[] = [];
 
-    while(true){
-      this.dbLocationsList.forEach(element => {
-        const from = new google.maps.LatLng(this.tempLocationList[this.tempLocationList.length-1]);
-        const to = new google.maps.LatLng(element.lat, element.lng);
-        let distance = google.maps.geometry.spherical.computeDistanceBetween(from, to);
-        distanceList.push(distance);
-      });
-      this.minDestinationIndex = distanceList.indexOf(Math.min(...distanceList));
-      this.tempLocationList.push(this.dbLocationsList[this.minDestinationIndex]);
-      this.dbLocationsList = this.dbLocationsList.filter(item => item !== this.dbLocationsList[this.minDestinationIndex]);
-      distanceList.splice(0,distanceList.length)
-      if(this.dbLocationsList.length == 0)  break;
-    }
- 
-    for(let i= 0; i< this.tempLocationList.length; i++){
-      if(i==this.tempLocationList.length-1 && i==0) continue; //remove origin & destination
-      else{
-        this.waypoints.push({
-          location: this.tempLocationList[i],
-          stopover: false,
-        })
-      }
-    }
+      while(this.dbLocationsList.length != 0){
+        
+        this.dbLocationsList.forEach(element => {
+          const from = new google.maps.LatLng(this.tempLocationList[this.tempLocationList.length-1]);
+          const to = new google.maps.LatLng(element.lat, element.lng);
+          let distance = google.maps.geometry.spherical.computeDistanceBetween(from, to);
+          distanceList.push(distance);
+        });
 
+        this.minDestinationIndex = distanceList.indexOf(Math.min(...distanceList));
+        this.tempLocationList.push(this.dbLocationsList[this.minDestinationIndex]);
+        this.dbLocationsList = this.dbLocationsList.filter(item => item !== this.dbLocationsList[this.minDestinationIndex]);
+        distanceList.splice(0,distanceList.length);
+
+        service.route({
+          origin: this.tempLocationList[this.tempLocationList.length-2],
+          destination: this.tempLocationList[this.tempLocationList.length-1],
+          travelMode: google.maps.TravelMode.DRIVING ,
+          avoidHighways: true,
+          avoidTolls: true,
+        }, (data, status) => {
+          if (status === 'OK') {
+            this.duration += data.routes[0].legs[0].duration.value;
+            this.durationText = new Date(this.duration * 1000).toISOString().substr(11, 8) 
+          } else {
+             console.log("your error")
+          }
+        });
+
+      }
+    
+      for(let i= 0; i< this.tempLocationList.length; i++){
+        if(i==this.tempLocationList.length-1 && i==0) continue; //remove origin & destination
+        else{
+          this.waypoints.push({
+            location: this.tempLocationList[i],
+            stopover: false,
+          })
+        }
+      }
+    
   }  
 
   onMapReady(mapInstance) {
